9d0178a41b8f4af807111ab47cc47fd707f6a202
Nan Du <dunan00001@gmail.com>
2019-07-30 15:49:05 -0400

    prepare to modify predict for 3 frame files

--- predict.py
+++ predict.py
@@ -6,12 +6,25 @@ from utils import *
 from models import *
 import os
 import tqdm
+from apex import amp
+
+def load_checkpoint(checkpoint_path, model, optimizer=None, gpu=True):
+    if gpu:
+        state = torch.load(checkpoint_path)
+    else:
+        state = torch.load(checkpoint_path, map_location='cpu')
+    model.load_state_dict(state['state_dict'])
+    if optimizer:
+        optimizer.load_state_dict(state['optimizer'])
+    print('model loaded from %s' % checkpoint_path)
 
 
 def predict(args):
     model = PepCNN(num_class=args.num_classes)
-    load_checkpoint(args.checkpoint_path, model)
-    model.cuda()
+    load_checkpoint(args.checkpoint_path, model, gpu=False)
+    # model.cuda()
+    # optimizer = torch.optim.Adam(params=model.parameters())
+    # model, _ = amp.initialize(model, optimizer, opt_level="O1", verbosity=0)
     model.eval()
 
     probs = []
@@ -25,9 +38,10 @@ def predict(args):
     for batch in tqdm.tqdm(data_loader):
         feature, target = batch[0], batch[1]
         # feature.data.t_(), target.data.sub_(1)  # batch first, index align
-        feature, target = feature.cuda(), target.cuda()
+        # feature, target = feature.cuda(), target.cuda()
 
-        logit = data_parallel(model, feature)
+        # logit = data_parallel(model, feature)
+        logit = model(feature)
         prob = F.softmax(logit, 1)
 
         probs.append(prob.data.cpu().numpy())
@@ -42,6 +56,9 @@ def predict(args):
     size = len(data_loader.dataset)
     accuracy = 100 * corrects.data.cpu().numpy() / size
     print("acc: {:.4f}%({}/{})".format(accuracy, corrects, size))
+
+    """
+
     if args.predict_file:
         df = pd.read_csv(args.test_file, sep='\t', header=None)
         df["topps"] = topps
@@ -53,9 +70,10 @@ def predict(args):
     filename_base = os.path.splitext(os.path.basename(args.predict_file))[0]
     prob_path = os.path.join(parent_dir, filename_base+'.npy')
     np.save(prob_path, probs)
+    """
 
 
 if __name__ == '__main__':
     args = argparser()
-    assert args.predict_file is not None
+    # assert args.predict_file is not None
     predict(args)
